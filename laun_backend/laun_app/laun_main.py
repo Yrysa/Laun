3a4eac2bbfe1aac30f967e0565f7c75b8daf3c58
Yrysa <130750163+Yrysa@users.noreply.github.com>
2026-05-21 16:52:40 +0500

    Fix development CORS for frontend auth requests

--- laun_backend/laun_app/laun_main.py
+++ laun_backend/laun_app/laun_main.py
@@ -11,12 +11,22 @@ LaunBase.metadata.create_all(bind=laun_engine)
 
 app = FastAPI(title=laun_settings.PROJECT_NAME)
 
+laun_allowed_origins = [
+    laun_settings.FRONTEND_URL.rstrip("/"),
+    "http://localhost:5173",
+    "http://127.0.0.1:5173",
+    "http://localhost:3000",
+    "http://127.0.0.1:3000",
+]
+
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=[laun_settings.FRONTEND_URL, "http://localhost:5173", "http://127.0.0.1:5173"],
+    allow_origins=laun_allowed_origins,
+    allow_origin_regex=r"https?://(localhost|127\.0\.0\.1)(:\d+)?",
     allow_credentials=True,
-    allow_methods=["*"],
+    allow_methods=["GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"],
     allow_headers=["*"],
+    expose_headers=["*"],
 )
 
 app.include_router(laun_auth_router, prefix=laun_settings.API_PREFIX)
