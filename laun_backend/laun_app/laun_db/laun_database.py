71c1fe20e115145f8a68d14e4d9c5f6820ef9c4d
Yrysa <130750163+Yrysa@users.noreply.github.com>
2026-05-21 16:42:54 +0500

    Support SQLite connection args

--- laun_backend/laun_app/laun_db/laun_database.py
+++ laun_backend/laun_app/laun_db/laun_database.py
@@ -3,7 +3,8 @@ from sqlalchemy.orm import declarative_base, sessionmaker
 
 from laun_app.laun_core.laun_config import laun_settings
 
-laun_engine = create_engine(laun_settings.DATABASE_URL, pool_pre_ping=True)
+laun_connect_args = {"check_same_thread": False} if laun_settings.DATABASE_URL.startswith("sqlite") else {}
+laun_engine = create_engine(laun_settings.DATABASE_URL, pool_pre_ping=True, connect_args=laun_connect_args)
 LaunSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=laun_engine)
 LaunBase = declarative_base()
 
